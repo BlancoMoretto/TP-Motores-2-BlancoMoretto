38a61fd366dad25a29805a64163d05a954b4f1c4
Kurcolml <32647317+Kurcolml@users.noreply.github.com>
2017-10-24 17:07:52 -0300

    Herramienta que crea un scriptableObjec
    Utiliza los scriptableObject para generar "pinceles" que servirán para pintar texturas.

---
+++ Editor de pinceles/Assets/Brush Editor/Brush.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Renderer))]
+public class Brush : ScriptableObject {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
